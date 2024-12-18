4d9f71ffe6cc2e80f2ad7ae5c1d91b1bb7c4526e
arynch24 <arynch24@gmail.com>
2024-12-19 01:46:03 +0530

    notes added

--- react-p1/src/main.jsx
+++ react-p1/src/main.jsx
@@ -10,6 +10,8 @@ function MyApp(){
         </div>
     )
 }
+//created our own component with our format
+//which will only work in our custom render function made by us in CustomReact folder where we created our own kinda react library
 
 // const ReactElement = {
 //     type: 'a',
@@ -24,20 +26,33 @@ const anotherElement = (
     <a href="https://google.com" target='_blank'>Visit google</a>
 )
 
-
-
 const anotherUser = "chai aur react"
 
+/*
+src: https://github.com/facebook/react/blob/main/packages/react/src/jsx/ReactJSXElement.js
+export function createElement(type, config, children) {}
+*/
+
+//A react component is made in this format only 
+//this is in react format created by me which can be rendered using line 43 render method
 const reactElement = React.createElement(
-    'a',
-    {href: 'https://google.com',target: '_blank' },
-    'click me to visit google',
-    anotherElement
+    'a',//type
+    {href: 'https://google.com',target: '_blank' },//config
+    'click me to visit google',//children
+    anotherElement//must be an evaluated expression //children
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  reactElement
+
+  //or
  
-    reactElement
-  
+  // <>
+  // <App/>
+  // </>
+
+  //or
+
+  // App()
 )
 
