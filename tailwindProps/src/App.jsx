e5cabccb6061a9331e90dd494e72c4d4b2a17d6c
arynch24 <arynch24@gmail.com>
2024-12-19 15:44:33 +0530

    tailwind and properties

---
+++ tailwindProps/src/App.jsx
@@ -0,0 +1,14 @@
+import Card from './Card'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Card name="Monkey [Banar]" price ="$1000" />
+    <Card name="Monkey [Vanar]" price ="$2000"/>
+    </>
+  )
+}
+
+export default App
