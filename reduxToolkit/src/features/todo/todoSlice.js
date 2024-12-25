13c41944d37cc143a5f804ebfc730c2f9c035276
arynch24 <arynch24@gmail.com>
2024-12-25 17:34:35 +0530

    info added

--- reduxToolkit/src/features/todo/todoSlice.js
+++ reduxToolkit/src/features/todo/todoSlice.js
@@ -7,17 +7,22 @@ const initialState={
 }
 
 //create slice is a method
+//IN WHICH WE HAVE TO PASS AN OBJECT
 export const todoSlice = createSlice({
     name:'todo',
     initialState,
+    // Reducers are Functions that specify how the state changes in response to actions. Each reducer handles a specific action.
     reducers:{
         //addTodo(property) : reducer - function (state,action)
         //reducer is the method which contains state and action as arguments
         //state tells the initial or current state of the store
-        //actions tell what happened in the application ,only way to send data to redux store
+        //actions are objects that describe what happened in the application ,only way to send data to redux store
+
+        //key and function pair
         addTodo: (state,action)=>{
             const todo={
                 id:nanoid(),//for givind id automatically
+                //payload contains extra information about the state
                 text:action.payload.text
             }
 
@@ -28,7 +33,7 @@ export const todoSlice = createSlice({
 
             //overiding whole todos array with new array after deleting
             state.todos=state.todos.filter((todo)=>{
-                todo.id!==action.payload
+                todo.id!==action.payload.id
             })
         }
     }
